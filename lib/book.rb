61a097df01affabbe09d215cdc03d15c289f4853
Jennifer Bell & Abdul Rahimi <student@epicodus.com>
2015-08-19 11:09:29 -0700

    Add all method to book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,21 @@
 class Book
-  attr_reader(:title)
+  attr_reader(:title, :author, :id)
 
   define_method(:initialize) do |attr|
     @title = attr.fetch(:title)
+    @author = attr.fetch(:author)
+    @id = attr.fetch(:id)
+  end
+
+  def self.all
+    returned_books = DB.exec("SELECT * FROM books;")
+    books = []
+    returned_books.each() do |books|
+      title = books.fetch("title")
+      id = books.fetch("id").to_i()
+      author = books.fetch("author")
+      books.push(Book.new({:title => title, :id => id, :author => author}))
+    end
+    books
   end
 end # ends class
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,20 @@
 require ('rspec')
 require('book')
+require('pg')
+
+DB = PG.connect({:dbname => "library_system_test"})
 
 describe(Book) do
   describe('#title') do
     it("returns the title of the book") do
-      test_book = Book.new({:title => "Harry Potter and the muggle mortgage crisis"})
+      test_book = Book.new({:title => "Harry Potter and the muggle mortgage crisis", :author => "J.K. Meowling", :id => nil})
       expect(test_book.title()).to(eq("Harry Potter and the muggle mortgage crisis"))
+    end
+  end
+
+  describe('.all') do
+    it ('is empty at first') do
+      expect(Book.all()).to(eq([]))
     end
   end
 end #end spec
